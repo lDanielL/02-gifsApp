ee031ade78563a9217e5cc08c50998f42a9fc090
DanielL <rra.daniel@gmail.com>
2022-05-31 17:13:50 -0400

    Primer commit

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent  {
+
+  get historial(){
+    return 1;
+  }
+  constructor() { }
+
+
+
+}
